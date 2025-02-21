e2dcf8b42e8e3ed7dd99007786a2c59a49e2eda1
Godx-Hub <123382051+obfalchx@users.noreply.github.com>
2025-02-21 09:56:02 +0700

    Add files via upload

--- DoughKingFull.lua
+++ DoughKingFull.lua
@@ -1,3 +1,5 @@
+game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("SetTeam","Marines") 
+
 hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Death), function() end)
 hookfunction(require(game:GetService("ReplicatedStorage").Effect.Container.Respawn), function() end)
 hookfunction(require(game:GetService("ReplicatedStorage"):WaitForChild("GuideModule")).ChangeDisplayedNPC, function() end)
