aa4d231922062750f282178a02f539d1a5e7bd22
Godx-Hub <123382051+obfalchx@users.noreply.github.com>
2025-02-22 13:07:02 +0700

    Add files via upload

--- DoughKingFull.lua
+++ DoughKingFull.lua
@@ -475,6 +475,7 @@ spawn(function()
                     if doughKing then
                         topos(doughKing.HumanoidRootPart.CFrame * CFrame.new(5, 10, 7))
                     else
+                    wait(4)
                         loadstring(game:HttpGet("https://raw.githubusercontent.com/obfalchx/edit/refs/heads/main/Dough_King.txt"))()
                     end
                 end
